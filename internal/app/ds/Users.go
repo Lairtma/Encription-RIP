8a608d7500d80a47f496c1a7d1285b7124f812cb
Lairtma <lairty@mail.ru>
2024-09-18 03:21:24 +0300

    master cleaning

--- internal/app/ds/Users.go
+++
@@ -1,8 +0,0 @@
-package ds
-
-type User struct {
-	Id          uint   `gorm:"primaryKey"`
-	Login       string `gorm:"type:varchar(50)"`
-	Password    string `gorm:"type:varchar(50)"`
-	IsModerator bool   `gorm:"type:boolean"`
-}
